fd1a56943f0c14d02ee640bfa5981360e712a4db
Kolja Kube <code@koljaku.be>
2017-10-17 17:54:54 +0200

    Install homebrew-managed fonts to system.
    Sharing fonts between users can be done this way. Installing twice into ~/Library confuses homebrew (“already installed”).

--- config.fish
+++ config.fish
@@ -1,2 +1,5 @@
+# Install fonts for all users.
+set -gx HOMEBREW_CASK_OPTS --fontdir=/Library/Fonts
+
 # iTerm2's shell integration (installed via iTerm2 itself).
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
