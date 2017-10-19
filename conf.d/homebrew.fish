abfbe06abdc6c457d221966527147e108e979ada
Kolja Kube <code@koljaku.be>
2017-10-19 07:20:57 +0200

    Use config directory instead of monolithic file.

---
+++ conf.d/homebrew.fish
@@ -0,0 +1,2 @@
+# Install fonts for all users.
+set -gx HOMEBREW_CASK_OPTS --fontdir=/Library/Fonts
---
+++ conf.d/iterm2.fish
@@ -0,0 +1,2 @@
+# iTerm2's shell integration (installed via iTerm2 itself).
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
